ac5271c62b9498c4773aa2fbf91e543288c4bbd4
avigeilpro <avigeilpro@users.noreply.github.com>
2016-02-21 17:02:07 +0100

    Create button_class.h

---
+++ firmware/button_class.h
@@ -0,0 +1,28 @@
+#ifndef button_class
+#define button_class
+
+#include "application.h"
+#include "DigoleSerialDisp.h"
+
+class Button {
+private:
+    int but_cord[10][4];
+    char but_text[10][20];
+    int but_text_pos[10][3];
+    int but_color[10][2];
+    int but_count;
+protected:
+    
+public:
+    Button();
+    void init(int count,int *cord,char *text,int *position,int *color);
+    void draw(int num);
+    void drawClick(int num);
+    void drawAll(void);
+    int CheckButtons(void (*but_click)(int),void (*out_but)(int,int));
+    int BackColor=0;
+    DigoleSerialDisp *mydisp;
+};
+
+
+#endif

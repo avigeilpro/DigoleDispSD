c839b832b1da9fdf14d17809ffd3b9ccfd039d89
avigeilpro <avigeilpro@users.noreply.github.com>
2016-03-02 06:32:56 +0100

    Update button_class.cpp

--- firmware/button_class.cpp
+++ firmware/button_class.cpp
@@ -44,25 +44,25 @@ void Button::draw(int num){
         int btnbackColor=but_color[num][0];
         int btnColor=but_color[num][1];
         
-        mydisp->setColor(btnColor);
-        mydisp->drawBox(x,y,w,h);
-        mydisp->setColor(btnbackColor);
-        mydisp->drawFrame(x + 5,y + 5,w - 10,h - 10);
+        Display.setColor(btnColor);
+        Display.drawBox(x,y,w,h);
+        Display.setColor(btnbackColor);
+        Display.drawFrame(x + 5,y + 5,w - 10,h - 10);
         
-        mydisp->drawLine(x + 1,y+1,x + 5,y + 5);
-        mydisp->drawLine(x+w-1,y+1,x+w - 5,y + 5);
-        mydisp->drawLine(x+1,y+h-1,x+5,y+h-5);
-        mydisp->drawLine(x+w-1,y+h-1,x+w-5,y+h-5);
+        Display.drawLine(x + 1,y+1,x + 5,y + 5);
+        Display.drawLine(x+w-1,y+1,x+w - 5,y + 5);
+        Display.drawLine(x+1,y+h-1,x+5,y+h-5);
+        Display.drawLine(x+w-1,y+h-1,x+w-5,y+h-5);
         
-        mydisp->setColor(btnColor);
-        mydisp->setBgColor();
-        mydisp->setColor(btnbackColor);
-        mydisp->setFont(but_text_pos[num][2]);
-        mydisp->drawAbsStr(x+but_text_pos[num][0],y+but_text_pos[num][1],but_text[num]);
+        Display.setColor(btnColor);
+        Display.setBgColor();
+        Display.setColor(btnbackColor);
+        Display.setFont(but_text_pos[num][2]);
+        Display.drawAbsStr(x+but_text_pos[num][0],y+but_text_pos[num][1],but_text[num]);
         
-        mydisp->setColor(BackColor);
-        mydisp->setBgColor();
-        mydisp->setColor(1);
+        Display.setColor(BackColor);
+        Display.setBgColor();
+        Display.setColor(1);
 
 }
 void Button::drawClick(int num){
@@ -73,39 +73,39 @@ void Button::drawClick(int num){
         int h=but_cord[num][3];
         int btnColor=but_color[num][1];
 
-        mydisp->setColor(0);
-        mydisp->drawFrame(x + 5,y + 5,w - 10,h - 10);
+        Display.setColor(0);
+        Display.drawFrame(x + 5,y + 5,w - 10,h - 10);
         
-        mydisp->drawLine(x + 1,y+1,x + 5,y + 5);
-        mydisp->drawLine(x+w-1,y+1,x+w - 5,y + 5);
-        mydisp->drawLine(x+1,y+h-1,x+5,y+h-5);
-        mydisp->drawLine(x+w-1,y+h-1,x+w-5,y+h-5);
+        Display.drawLine(x + 1,y+1,x + 5,y + 5);
+        Display.drawLine(x+w-1,y+1,x+w - 5,y + 5);
+        Display.drawLine(x+1,y+h-1,x+5,y+h-5);
+        Display.drawLine(x+w-1,y+h-1,x+w-5,y+h-5);
         
-        mydisp->setColor(btnColor);
-        mydisp->setBgColor();
-        mydisp->setColor(0);
-        mydisp->setFont(but_text_pos[num][2]);
-        mydisp->drawAbsStr(x+but_text_pos[num][0],y+but_text_pos[num][1],but_text[num]);
+        Display.setColor(btnColor);
+        Display.setBgColor();
+        Display.setColor(0);
+        Display.setFont(but_text_pos[num][2]);
+        Display.drawAbsStr(x+but_text_pos[num][0],y+but_text_pos[num][1],but_text[num]);
         
         delay(100);
         
-        mydisp->setColor(0x6F);
-        mydisp->drawFrame(x + 5,y + 5,w - 10,h - 10);
+        Display.setColor(0x6F);
+        Display.drawFrame(x + 5,y + 5,w - 10,h - 10);
         
-        mydisp->drawLine(x + 1,y+1,x + 5,y + 5);
-        mydisp->drawLine(x+w-1,y+1,x+w - 5,y + 5);
-        mydisp->drawLine(x+1,y+h-1,x+5,y+h-5);
-        mydisp->drawLine(x+w-1,y+h-1,x+w-5,y+h-5);
+        Display.drawLine(x + 1,y+1,x + 5,y + 5);
+        Display.drawLine(x+w-1,y+1,x+w - 5,y + 5);
+        Display.drawLine(x+1,y+h-1,x+5,y+h-5);
+        Display.drawLine(x+w-1,y+h-1,x+w-5,y+h-5);
         
-        mydisp->setColor(btnColor);
-        mydisp->setBgColor();
-        mydisp->setColor(0x6F);
-        mydisp->setFont(but_text_pos[num][2]);
-        mydisp->drawAbsStr(x+but_text_pos[num][0],y+but_text_pos[num][1],but_text[num]);
+        Display.setColor(btnColor);
+        Display.setBgColor();
+        Display.setColor(0x6F);
+        Display.setFont(but_text_pos[num][2]);
+        Display.drawAbsStr(x+but_text_pos[num][0],y+but_text_pos[num][1],but_text[num]);
         
-        mydisp->setColor(BackColor);
-        mydisp->setBgColor();
-        mydisp->setColor(1);
+        Display.setColor(BackColor);
+        Display.setBgColor();
+        Display.setColor(1);
 
 }
 
@@ -113,15 +113,15 @@ int Button::CheckButtons(void (*but_click)(int),void (*out_but)(int,int)){
     
     int x,y,a,b,c,d;
     int but=-1;
-    mydisp->readTouchScreen();
+    Display.readTouchScreen();
 
-    x = mydisp->read1() << 8;
-    x |= mydisp->read1();
-    y = mydisp->read1() << 8;
-    y |= mydisp->read1();
+    x = Display.read1() << 8;
+    x |= Display.read1();
+    y = Display.read1() << 8;
+    y |= Display.read1();
 
     //évite le plantage
-    mydisp->write((uint8_t)0);
+    Display.write((uint8_t)0);
 
     //click sur l'écran
     if ((x<320)&&(y<240)){
@@ -145,7 +145,3 @@ int Button::CheckButtons(void (*but_click)(int),void (*out_but)(int,int)){
     }
     return but;
 }
-
-
-
-
